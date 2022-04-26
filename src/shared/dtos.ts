dbac3f4875ee1baf10eab2de551d74dde82b1e72
misostack <misostack.com@gmail.com>
2022-04-26 16:08:10 +0700

    keep IsEmpty validator from class validator

--- src/shared/dtos.ts
+++ src/shared/dtos.ts
@@ -1,22 +1,28 @@
 import { registerDecorator, ValidationOptions } from 'class-validator';
 import { isEmpty } from 'lodash';
 
-import { IsEnum, IsOptional, IsUrl, MaxLength } from 'class-validator';
+import {
+  IsEnum,
+  IsOptional,
+  IsUrl,
+  MaxLength,
+  IsNotEmpty,
+} from 'class-validator';
 
-const IsNotEmpty = (validationOptions?: ValidationOptions) => {
-  return (object: Object, propertyName: string) => {
-    registerDecorator({
-      name: 'isNotEmpty',
-      target: object.constructor,
-      propertyName: propertyName,
-      options: validationOptions,
-      validator: {
-        validate(value: any) {
-          return !isEmpty(value);
-        },
-      },
-    });
-  };
-};
+// const IsNotEmpty = (validationOptions?: ValidationOptions) => {
+//   return (object: Object, propertyName: string) => {
+//     registerDecorator({
+//       name: 'isNotEmpty',
+//       target: object.constructor,
+//       propertyName: propertyName,
+//       options: validationOptions,
+//       validator: {
+//         validate(value: any) {
+//           return !isEmpty(value);
+//         },
+//       },
+//     });
+//   };
+// };
 
 export { IsEnum, IsNotEmpty, IsOptional, IsUrl, MaxLength };
