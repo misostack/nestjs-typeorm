86fd0370c4a9f881e6e919e871c0407fa63f8119
misostack <misostack.com@gmail.com>
2022-07-07 17:20:33 +0700

    update typeorm cli

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
 export const DATABASE_CONFIG = {
   MAIN_DB_TYPE: 'mysql',
   MAIN_DB_URL: process.env.MYSQL_DB_URL || 'nestjs',
@@ -23,5 +24,24 @@ export class Environment {
         migrationsDir: dirname + '/database/migrations',
       },
     };
+  }
+  static getMainDatasourceConfiguration(dirname): any {
+    return new DataSource({
+      migrationsTableName: 'migrations',
+      type: DATABASE_CONFIG.MAIN_DB_TYPE,
+      url: DATABASE_CONFIG.MAIN_DB_URL,
+      entities: [dirname + '/**/entities/*.entity{.js,.ts}'],
+      // MYSQL will store Timestamp in GMT ( UTC = 0)
+      timezone: 'Z', // must have this, if the response date will be marked as current timezone
+      charset: DATABASE_CONFIG.MAIN_DB_CHARSET,
+      // must not be synchronize automaticall, use data migration instea
+      synchronize: false,
+      // migrations
+      migrations: [dirname + '/database/migrations/*.ts'],
+      // cli
+      cli: {
+        migrationsDir: dirname + '/database/migrations',
+      },
+    } as DataSourceOptions);
   }
 }
